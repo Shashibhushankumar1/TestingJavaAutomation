001c9e63318e633932d7b12b096fba3a5b824139
Shashibhushankumar1 <shashikohli594@gmail.com>
2025-07-24 13:50:08 +0700

    Take screenshot failure steps scenario in extend report

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,8 +1,16 @@
 package stepDefinitions;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 import utils.TestContextSetup;
 
 public class Hooks {
@@ -20,5 +28,20 @@ TestContextSetup testContextSetup;
 		//3)Then you can Access TestBase WebDriverManager
 		
 		testContextSetup.testBase.WebDriverManager().quit();
+	}
+	//It will execute feature file written every scenario check any failure then take screenshot
+	@AfterStep
+	public void AddScreenshot(Scenario scenario) throws IOException
+	{
+		WebDriver driver =testContextSetup.testBase.WebDriverManager();
+		if(scenario.isFailed())
+		{
+		//screenshot
+		File sourcePath= 	((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		byte[] fileContent = FileUtils.readFileToByteArray(sourcePath);
+		scenario.attach(fileContent, "image/png", "image");
+		
+		}
+		
 	}
 }
