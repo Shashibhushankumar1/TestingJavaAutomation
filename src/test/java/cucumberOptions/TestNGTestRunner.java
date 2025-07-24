2d6178687be8aedcb753356d49774f851931b7ca
Shashibhushankumar1 <shashikohli594@gmail.com>
2025-07-24 13:21:07 +0700

    extend report in details with pom.xml & TestRunner

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -7,7 +7,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",
 monochrome = true,tags ="@PlaceOrder or @OffersPage",
-plugin= {"html:target/cucumber.html", "json:target/cucumber.json"})
+plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+		})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	//Step :-7 (for run particular tag related scenarios Run for tags ="@PlaceOrder" only
 	//it will pick from feature file (Checkout.feature)  -->@PlaceOrder
