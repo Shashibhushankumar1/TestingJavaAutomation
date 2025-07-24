ab28ad54b8229f67546df377b57cedb576569b49
Shashibhushankumar1 <shashikohli594@gmail.com>
2025-07-24 14:03:15 +0700

    re-run failed test scenario

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -8,7 +8,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",
 monochrome = true,tags ="@PlaceOrder or @OffersPage",
 plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"rerun:target/failed_scenarios.txt"
 		})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	//Step :-7 (for run particular tag related scenarios Run for tags ="@PlaceOrder" only
