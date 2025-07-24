772ac07897b4007e1903d8659a04d42d73c5f2c1
Shashibhushankumar1 <shashikohli594@gmail.com>
2025-07-24 12:25:17 +0700

    html ,json report in TestNgRunner

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -5,7 +5,9 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true,tags ="@PlaceOrder")
+@CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",
+monochrome = true,tags ="@PlaceOrder or @OffersPage",
+plugin= {"html:target/cucumber.html", "json:target/cucumber.json"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	//Step :-7 (for run particular tag related scenarios Run for tags ="@PlaceOrder" only
 	//it will pick from feature file (Checkout.feature)  -->@PlaceOrder
